3763f8577f889f778087bf9748939e80d21b5103
SaneStreet <MichaelHansenIT@live.dk>
2022-03-14 14:26:02 +0100

    initial commit

---
+++ chapters/classes.php
@@ -0,0 +1,24 @@
+<?php 
+
+    class User {
+
+        // properties and methods here
+
+    }
+
+    $userOne = new User();
+    $userTwo = new User();
+
+    echo 'Class is ' . get_class($userOne);
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>OOP PHP - Index</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
