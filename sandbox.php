2633c8cdf26ea9e4c9d1bea1e375fa1827a02722
SaneStreet <MichaelHansenIT@live.dk>
2022-03-15 13:32:44 +0100

    Chapter 12 - Validation (Part 1)
    Added a form on sandbox page and some styling.

--- sandbox.php
+++ sandbox.php
@@ -1,14 +1,29 @@
 <?php 
 
-    
+    if(isset($_POST['submit'])){
+        echo "Form submitted.";
+    }
 
 ?>
 
 <html lang="en">
     <head>
-        <title>PHP OOP - Sandbox</title>
+        <title>PHP OOP - Validation (Part 1)</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
     </head>
     <body>
-        <p>Where the magic happens.</p>
+        <div class="new-user">
+            <h2>Create new user</h2>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+
+                <label>Username:</label>
+                <input type="text" name="username">
+
+                <label>Email:</label>
+                <input type="text" name="email">
+
+                <input type="submit" value="Submit" name="submit">
+            </form>
+        </div>
     </body>
 </html>
